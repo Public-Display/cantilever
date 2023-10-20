a5cb3fa8a7cce4039b74897ac009e1331287d7dc
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2023-10-20 21:02:34 +0100

    uppercase fixes

--- src/App.jsx
+++ src/App.jsx
@@ -70,14 +70,14 @@ function App() {
         className='overflow-hidden h-full md:min-h-[120dvh] w-full md:w-2/5 flex items-center justify-end absolute md:relative text-yellow-600'
       >
         <div className='absolute left-0 md:right-24 pl-8 pt-72 md:pt-0 md-pl-0 opacity-100 md:left-2/3 flex items-center justify-center flex-col text-white'>
-          <p className='text-xl md:text-2xl whitespace-nowrap'>Ready in</p>
+          <p className='text-xl md:text-2xl whitespace-nowrap uppercase'>Ready in</p>
           <div className='text-6xl md:text-8xl font-bold text-cyellow-100 flex items-center space-x-2 body-font font-albert'>
             <span>{countdownTime?.countdownDays ?? '...'}</span>
             {/* <span>{countdownTime?.countdownHours}</span>
             <span>{countdownTime?.countdownlMinutes}</span>
             <span>{countdownTime?.countdownSeconds}</span> */}
           </div>
-          <p className='text-2xl md:text-4xl text-cyellow-100 body-font font-albert font-normal'>Days</p>
+          <p className='text-2xl md:text-4xl text-cyellow-100 body-font font-albert font-normal uppercase'>Days</p>
         </div>
       </div>
 
